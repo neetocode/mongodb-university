53b87d99bdeaed18a2973a31ecbfb830ebbcbd08
Miguel Neto <miguelrsn@gmail.com>
2017-10-30 11:00:29 -0300

    m121 lab02

---
+++ m121/chapter03/lab02.js
@@ -0,0 +1,42 @@
+/*
+
+Lab - $unwind
+
+
+Let's use our increasing knowledge of the Aggregation Framework to explore our movies collection in more detail. We'd like to calculate how many movies every cast member has been in and get an average imdb.rating for each cast member.
+
+What is the name, number of movies, and average rating (truncated to one decimal) for the cast member that has been in the most number of movies with English as an available language?
+
+Provide the input in the following order and format
+
+{ "_id": "First Last", "numFilms": 1, "average": 1.1 }
+*/
+
+
+var pipeline = [
+    {
+        $match: {
+            'languages': 'English',
+            'imdb.rating': { $gte: 0 }
+        }
+    },
+    {
+        $unwind: '$cast'
+    },
+    {
+        $group: {
+            _id: '$cast',
+            numFilms: { $sum: 1 },
+            average: { $avg: '$imdb.rating' }
+        }
+    },
+    {
+        $sort: {
+            "numFilms": -1
+        }
+    },
+    {
+        $limit: 1
+    }
+
+]
