f9190b2e6f2496e982250271baf9b6d92cbad83a
Miguel Neto <miguelrsn@gmail.com>
2017-11-05 22:59:19 -0300

    initial lab

---
+++ m121/chapter04/lab01.js
@@ -0,0 +1,5 @@
+/**
+ * Lab - $facets
+ * How many movies are in both the top ten highest rated movies according to the imdb.rating and the metacritic fields? We should get these results with exactly one access to the database.
+ */
+
