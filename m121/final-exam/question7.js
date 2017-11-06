1236efdb9a6aeb0b2b5aaa62f7f1023c4a6df8ed
Miguel Neto <miguelrsn@gmail.com>
2017-11-06 11:25:42 -0300

    m121. final exam. question7

---
+++ m121/final-exam/question7.js
@@ -0,0 +1,32 @@
+
+
+var pipeline = [
+    {
+        $match: {
+            $or: [
+                {
+                    $and: [
+                        { src_airport: 'JFK' }, { dst_airport: 'LHR' }
+                    ],
+                    $and: [
+                        { src_airport: 'LHR' }, { dst_airport: 'JFK' }
+                    ]
+                }
+            ]
+        }
+    },
+    {
+        $lookup: {
+            from: 'air_alliances',
+            localField: 'airline.name',
+            foreignField: 'airlines',
+            as: 'alliance'
+        }
+    },
+    {
+        $group: {
+            _id: '$alliance.name',
+            sum: { $sum: 1 }
+        }
+    }
+]
